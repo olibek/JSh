cd847f0e6fb18ae9aa54fa30a0ae9053e5b4c73a
Alibek Botashev <botashev_alibek@mail.ru>
2021-07-30 14:41:14 +0300

    add lesson 2 hard

--- script/script.js
+++ script/script.js
@@ -1,1 +1,11 @@
 let num = 266219;
+let summ = 1;
+num = String(num);
+num = num.split('');
+let numb = num.map(Number);
+for (let i = 0; i < numb.length; ++i) {
+  summ *= numb[i];
+}
+console.log(summ);
+summ = summ ** 3;
+console.log(String(summ).slice(0, 2));
