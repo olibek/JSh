698821487a78f85b25b461c2c51337f7df58aab3
Alibek Botashev <botashev_alibek@mail.ru>
2021-08-01 10:53:39 +0300

    feat(main.js):fix code(reduce)

--- script/script.js
+++ script/script.js
@@ -1,11 +1,22 @@
+"use strict";
 let num = 266219;
-let summ = 1;
+
 num = String(num);
 num = num.split('');
 let numb = num.map(Number);
-for (let i = 0; i < numb.length; ++i) {
-  summ *= numb[i];
-}
-console.log(summ);
-summ **= 3;
-console.log(String(summ).slice(0, 2));
+
+// for (let i = 0; i < numb.length; ++i) {
+//   summ *= numb[i];
+// }
+
+// console.log(summ);
+// summ **= 3;
+// console.log(String(summ).slice(0, 2));
+
+let sum = numb.reduce((prev, item, index) => {
+  return prev * item;
+});
+console.log(sum);
+
+sum **= 3;
+console.log(String(sum).slice(0, 2));
