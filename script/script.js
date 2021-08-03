ef83feaf93a1817ce1a72bcfdee2c142c0631578
Alibek Botashev <botashev_alibek@mail.ru>
2021-08-04 00:07:59 +0300

    feat(script.js): add new feat

--- script/script.js
+++ script/script.js
@@ -1,31 +1,16 @@
 'use strict';
-
-let lang = prompt('Введите язык');
-let day = {
-  ru: ['Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье'],
-  en: ['Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday']
+const fooString = function () {
+  let mess = prompt('Ввидети аргумент!');
+  if (typeof mess !== 'string') {
+    alert('Введите строку');
+  }
+  else {
+    mess = mess.trim();
+    console.log(mess, mess.length);
+    if (mess.length > 30) {
+      mess = mess.substr(0, 30) + '...';
+    }
+  }
+  console.log(mess);
 };
-
-if (lang === 'ru') {
-  console.log(day['ru']);
-}
-else if (lang === 'en') {
-  console.log(day['en']);
-}
-
-switch (lang) {
-  case 'en':
-    console.log(day['en']);
-    break;
-  case 'ru':
-    console.log(day['ru']);
-    break;
-}
-
-console.log(day[lang]);
-
-let namePerson = prompt('Ввидите имя');
-let result = namePerson === 'Артем' ? 'Директор' :
-  namePerson === 'Максим' ? 'Преподаватель' :
-    'Студент';
-console.log('Привет ' + result);
+fooString();
